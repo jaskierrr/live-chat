f9af6ba2a24f7e215f683ccb8714e52a8f0b315a
jaskierrr <mr.xaski.2002@gmail.com>
2025-02-11 13:50:59 +0700

    test

--- app/handlers/main.py
+++ app/handlers/main.py
@@ -3,7 +3,24 @@ import app.usecases.main as usecases
 
 router = APIRouter()
 
-@router.get('/user/{id}')
+
+@router.get("/user/{id}")
 async def get_user(id: int):
     res = usecases.get_user(id)
     return res
+
+
+router2 = APIRouter()
+
+
+@router2.get("/task/{id}")
+async def get_task(id: int):
+    return "task №" + str(id)
+
+
+router3 = APIRouter()
+
+
+@router3.get("/profile/{id}")
+async def get_profile(id: int):
+    return "profile №" + str(id)
--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from app.handlers.main import router
+from app.handlers.main import router, router2, router3
 
 app = FastAPI()
-
 app.include_router(router)
+app.include_router(router2)
+app.include_router(router3)
